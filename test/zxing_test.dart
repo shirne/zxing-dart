3b9a4840ed93547f85750910bc1b8d2aca0317d9
shirne <shirne@126.com>
2021-06-06 17:15:28 +0800

    test

--- test/zxing_test.dart
+++ test/zxing_test.dart
@@ -3,14 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:zxing/zxing.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-
-  test('test List', () {
+  test('test zxing', () {
     List<int> a = [];
     a.addAll(List.generate(10, (index) => 0));
     a.fillRange(0, 10, 1);
